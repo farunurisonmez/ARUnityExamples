f4ccf875005ad93218f9bc54e8e49f2bd816378e
Faru Nuri Sönmez <info@farunurisonmez.com>
2020-10-01 10:03:07 +0300

    Updating Compass AR Example to Unity 2020.1.6f1
    Updating Compass AR Example to AR Foundation 3.1.0 | Updating AR Foundation 3.1.0 packages to Unity 2020.1.6f1
    
    Co-Authored-By: Faru Nuri Sönmez <23061257+farunurisonmez@users.noreply.github.com>

---
+++ Assets/Scripts/GPSManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class GPSManager : MonoBehaviour
+{
+    string message = "Initialising GPS...";
+
+    public Transform trackedImageTransform;
+    public bool pointerMarkerDetection;
+
+    public float accuracy = 5f;
+    public float displacement = 0f;
+    public GameObject needle;
+
+    public static float heading;
+    public static float distance;
+
+
+    private MultiImageTracker get_trackedImageTransform;
+    public GameObject GetMultiImageTracker;
+
+
+
+        // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(BeginGPS());
+        InvokeRepeating("CustomUpdate", 1, 0.3f);
+        get_trackedImageTransform = GetMultiImageTracker.GetComponent<MultiImageTracker>();
+    }
+
+    void CustomUpdate()
+    {
+        if(pointerMarkerDetection == true) {
+            heading = Input.compass.trueHeading;
+            needle.transform.localRotation = Quaternion.Euler(0, 0, Input.compass.trueHeading + trackedImageTransform.eulerAngles.y);
+            message = Quaternion.Euler(0, 0, Input.compass.trueHeading + trackedImageTransform.eulerAngles.y).ToString();
+        }
+        else if(pointerMarkerDetection == false) {
+            heading = Input.compass.trueHeading;
+            needle.transform.localRotation = Quaternion.Euler(0, 0, heading);
+        }
+    }
+
+    float Haversine(float lat1, float long1, float lat2, float long2)
+    {
+       
+        float earthRad = 6371000;
+        float lRad1 = lat1 * Mathf.Deg2Rad;
+        float lRad2 = lat2 * Mathf.Deg2Rad;
+        float dLat = (lat2 - lat1) * Mathf.Deg2Rad;
+        float dLong = (long2 - long1) * Mathf.Deg2Rad;
+        float a = Mathf.Sin(dLat / 2f) * Mathf.Sin(dLat / 2f) +
+            Mathf.Cos(lRad1) * Mathf.Cos(lRad2) *
+            Mathf.Sin(dLong / 2f) * Mathf.Sin(dLong / 2f);
+        float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+
+        return earthRad * c;
+    }
+
+
+    IEnumerator BeginGPS() {
+
+        message = "Connecting to Location Services...";
+
+        if (!Input.location.isEnabledByUser) {
+            message = "Location Services Not Enabled";
+            yield break;
+        } 
+        else {
+            message = "Location Services Enabled"; 
+        }
+
+        // Start service before querying location
+        Input.location.Start(accuracy, displacement);
+
+        // Wait until service initializes
+        int maxWait = 10;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 10 seconds
+        if (maxWait < 1) {
+            message = "Timed out";
+            yield break; 
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed) {
+            message = "Unable to determine device location";
+            yield break;
+        }
+        else {
+            Input.compass.enabled = true; // Used to enable or disable compass
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        //Input.location.Stop();
+    }
+
+    //private void OnGUI()
+    //{
+    //    GUI.skin.label.fontSize = 60;
+    //    GUI.Label(new Rect(30, 30, 1000, 1000), message);
+    //}
+}
---
+++ Assets/Scripts/MultiImageTracker.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class MultiImageTracker : MonoBehaviour, ISerializationCallbackReceiver
+{
+
+    string message = "Multi Image Scanning Starts";
+
+    GameObject Object;
+
+    // TODO: Getting More Information on Scanning
+    public Text trackingStateText; 
+
+    // TODO: Spawn an Object on the Scanned Image with Touch Method
+    public Vector3 touchPosition; 
+
+    //Used to associate an 'XRReferenceImage' with a Prefab by using the 'XRReferenceImage's' guid as a unique identifier for a particular reference image.
+    [Serializable]
+    struct NamedPrefab
+    {
+        // System.Guid isn't serializable, so we store the Guid as a string. At runtime, this is converted back to a System.Guid
+        public string imageGuid;
+        public GameObject imagePrefab;
+        public NamedPrefab(Guid guid, GameObject prefab)
+        {
+            imageGuid = guid.ToString();
+            imagePrefab = prefab;
+        }
+    }
+
+    [SerializeField]
+    [HideInInspector]
+    List<NamedPrefab> m_PrefabsList = new List<NamedPrefab>();
+
+    Dictionary<Guid, GameObject> m_PrefabsDictionary = new Dictionary<Guid, GameObject>();
+    Dictionary<Guid, GameObject> m_Instantiated = new Dictionary<Guid, GameObject>();
+    ARTrackedImageManager m_TrackedImageManager;
+
+    [SerializeField]
+    [Tooltip("Reference Image Library")]
+    XRReferenceImageLibrary m_ImageLibrary;
+
+    //Get the XRReferenceImageLibrary
+    public XRReferenceImageLibrary imageLibrary
+    {
+        get => m_ImageLibrary;
+        set => m_ImageLibrary = value;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        m_PrefabsList.Clear();
+        foreach (var kvp in m_PrefabsDictionary)
+        {
+            m_PrefabsList.Add(new NamedPrefab(kvp.Key, kvp.Value));
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        m_PrefabsDictionary = new Dictionary<Guid, GameObject>();
+        foreach (var entry in m_PrefabsList)
+        {
+            m_PrefabsDictionary.Add(Guid.Parse(entry.imageGuid), entry.imagePrefab);
+        }
+    }
+
+    void Awake()
+    {
+        ar_RaycastManager = GetComponent<ARRaycastManager>(); //Detected features in the physical environmen
+        trackingStateText = GameObject.Find("trackingStateText").GetComponent<Text>();
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition) {
+        // TODO: Spawn an Object on the Scanned Image with Touch Method 
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    private void Update() {
+        Object = GameObject.FindGameObjectWithTag("Compass"); // Returns one active Compass Object tagged tag.
+    }
+
+    void OnEnable() {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable() {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs) {
+        foreach (var trackedImage in eventArgs.added) {
+            // Give the initial image a reasonable default scale
+            var minLocalScalar = Mathf.Min(trackedImage.size.x, trackedImage.size.y) / 10;
+            trackedImage.transform.localScale = new Vector3(minLocalScalar, minLocalScalar, minLocalScalar);
+            AssignPrefab(trackedImage);
+        }
+        foreach (ARTrackedImage updatedImage in eventArgs.updated) {
+            var minLocalScalar = Mathf.Min(updatedImage.size.x, updatedImage.size.y) / 10;
+            updatedImage.transform.localScale = new Vector3(minLocalScalar, minLocalScalar, minLocalScalar);
+
+            if (updatedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking) {
+                //Tracking is working normally.
+                updatePrefab(updatedImage);
+            }
+            else if (updatedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited) {
+                //Some tracking information is available, but it is limited or of poor quality.
+                updateLimitedPrefab(updatedImage);
+            }
+            else if (updatedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.None) {
+                notTracking(updatedImage);
+            }
+        }
+        foreach (ARTrackedImage removedImage in eventArgs.removed) {
+            // TODO: Apply Method to Destroy Object 
+        }
+    }
+
+    void AssignPrefab(ARTrackedImage trackedImage) {
+        if (m_PrefabsDictionary.TryGetValue(trackedImage.referenceImage.guid, out var prefab))
+            m_Instantiated[trackedImage.referenceImage.guid] = Instantiate(prefab, trackedImage.transform);
+    }
+
+    void updatePrefab(ARTrackedImage updatedImage) {
+        trackingStateText.text = "Marker Tracked";
+        trackingStateText.color = Color.green;
+        m_Instantiated[updatedImage.referenceImage.guid].SetActive(true);
+        if (updatedImage.referenceImage.name == "pointerMarker") {
+            Object.GetComponent<GPSManager>().trackedImageTransform = updatedImage.transform;
+            Object.GetComponent<GPSManager>().pointerMarkerDetection = true;
+        }
+    }
+
+    // TODO: Spawn an Object on the Scanned Image with Touch Method
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private ARRaycastManager ar_RaycastManager;
+
+
+    void updateLimitedPrefab(ARTrackedImage updatedImage) {
+
+        if (m_PrefabsDictionary.TryGetValue(updatedImage.referenceImage.guid, out var prefab)) {
+            trackingStateText.text = "Marker Not Tracked";
+            trackingStateText.color = Color.red;
+            m_Instantiated[updatedImage.referenceImage.guid].SetActive(false);
+        }
+    }
+
+    void notTracking(ARTrackedImage updatedImage) {
+
+    }
+
+    void destroyObject(ARTrackedImage removedImage) {
+
+    }
+    
+    public GameObject GetPrefabForReferenceImage(XRReferenceImage referenceImage)
+        => m_PrefabsDictionary.TryGetValue(referenceImage.guid, out var prefab) ? prefab : null;
+
+    public void SetPrefabForReferenceImage(XRReferenceImage referenceImage, GameObject alternativePrefab) {
+        m_PrefabsDictionary[referenceImage.guid] = alternativePrefab;
+        if (m_Instantiated.TryGetValue(referenceImage.guid, out var instantiatedPrefab)) {
+            m_Instantiated[referenceImage.guid] = Instantiate(alternativePrefab, instantiatedPrefab.transform.parent);
+            Destroy(instantiatedPrefab);
+        }
+    }
+
+#if UNITY_EDITOR
+    /// <summary>
+    /// This customizes the inspector component and updates the prefab list when
+    /// the reference image library is changed.
+    /// </summary>
+    [CustomEditor(typeof(MultiImageTracker))]
+    class PrefabImagePairManagerInspector : Editor
+    {
+        List<XRReferenceImage> m_ReferenceImages = new List<XRReferenceImage>();
+        bool m_IsExpanded = true;
+
+        bool HasLibraryChanged(XRReferenceImageLibrary library)
+        {
+            if (library == null)
+                return m_ReferenceImages.Count == 0;
+
+            if (m_ReferenceImages.Count != library.count)
+                return true;
+
+            for (int i = 0; i < library.count; i++)
+            {
+                if (m_ReferenceImages[i] != library[i])
+                    return true;
+            }
+
+            return false;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            //customized inspector
+            var behaviour = serializedObject.targetObject as MultiImageTracker;
+
+            serializedObject.Update();
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Script"));
+            }
+
+            var libraryProperty = serializedObject.FindProperty(nameof(m_ImageLibrary));
+            EditorGUILayout.PropertyField(libraryProperty);
+            var library = libraryProperty.objectReferenceValue as XRReferenceImageLibrary;
+
+            //check library changes
+            if (HasLibraryChanged(library))
+            {
+                if (library)
+                {
+                    var tempDictionary = new Dictionary<Guid, GameObject>();
+                    foreach (var referenceImage in library)
+                    {
+                        tempDictionary.Add(referenceImage.guid, behaviour.GetPrefabForReferenceImage(referenceImage));
+                    }
+                    behaviour.m_PrefabsDictionary = tempDictionary;
+                }
+            }
+
+            // update current
+            m_ReferenceImages.Clear();
+            if (library)
+            {
+                foreach (var referenceImage in library)
+                {
+                    m_ReferenceImages.Add(referenceImage);
+                }
+            }
+
+            //show prefab list
+            m_IsExpanded = EditorGUILayout.Foldout(m_IsExpanded, "Prefab List");
+            if (m_IsExpanded)
+            {
+                using (new EditorGUI.IndentLevelScope())
+                {
+                    EditorGUI.BeginChangeCheck();
+
+                    var tempDictionary = new Dictionary<Guid, GameObject>();
+                    foreach (var image in library)
+                    {
+                        var prefab = (GameObject)EditorGUILayout.ObjectField(image.name, behaviour.m_PrefabsDictionary[image.guid], typeof(GameObject), false);
+                        tempDictionary.Add(image.guid, prefab);
+                    }
+
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        Undo.RecordObject(target, "Update Prefab");
+                        behaviour.m_PrefabsDictionary = tempDictionary;
+                        EditorUtility.SetDirty(target);
+                    }
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 60;
+        GUI.Label(new Rect(30, 30, 1000, 1000), message);
+    }
+}
+
+
